2b9d71995712d77bdf5d06261a5e3e410de9b585
devDenis <dandenisedu@gmail.com>
2022-06-16 13:03:21 -0400

    Atualizações da pagina Home, incrementação de conteudos explicativos e botões para as redes sociais

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './homeStyle.css';
 import perfilImg from '../../assets/logo-perfil.png'
+import firstProject from '../../assets/firstProject.jpg'
+import { FiGithub } from 'react-icons/fi'
+import { FiLinkedin } from 'react-icons/fi'
+import { IoLogoWhatsapp } from 'react-icons/io'
 
 export default function Home() {
 	return (
@@ -26,11 +30,19 @@ export default function Home() {
 			<div className="explaining-container">
 				<span className="explaining-text">
 
-				<img src="" alt="" className="explaining-img" />
+				<img src={firstProject} alt="" className="explaining-img" />
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
 					Minus dolore expedita sequi aliquam eaque deserunt provident voluptates a, 
 					itaque voluptate ea culpa recusandae consequatur vero fugiat libero ipsum veniam accusamus?
 				</span>
+			</div>
+			
+			<div className="contact-me">
+				<div className="contacts">
+					<a href="https://github.com/DenisEdSilva" className="contact"><FiGithub /></a>
+					<a href="https://www.linkedin.com/in/denis-eduardo-da-silva-37a344185/" className="contact"><FiLinkedin /></a>
+					<a href="https://api.whatsapp.com/send?phone=5566996608693" className="contact"><IoLogoWhatsapp /></a>
+				</div>
 			</div>
 			
 		</div>
